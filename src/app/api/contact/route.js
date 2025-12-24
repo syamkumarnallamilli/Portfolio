11d40534140f7eb0aecdf7b4b4e9616f63e1ca42
syamkumarnallamilli <syamkumarnallamilli0@gmail.com>
2025-12-24 22:02:56 +0530

    change-1

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,14 +1,14 @@
 export async function POST(req) {
   const body = await req.text();
 
-  const res = await fetch(process.env.NEXT_PUBLIC_SCRIPT_URL, {
+  const res = await fetch(process.env.SCRIPT_URL, {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body,
+    body:new URLSearchParams(body).toString(),
   });
 
   const data = await res.text();
   return new Response(data, {
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "text/plain" },
   });
 }
